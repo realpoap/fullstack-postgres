af43af09f20c9aa56279f96cf14f405762c95cbb
realpoap <alapetite.mathieu@gmail.com>
2025-07-15 09:42:39 -0400

    course content part b

--- sequelize/index.js
+++ sequelize/index.js
@@ -1,55 +1,20 @@
-require('dotenv').config({ path: '.env.local' });
-const { Sequelize, Model, DataTypes } = require('sequelize')
-
 const express = require('express')
 const app = express()
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-	dialect: 'postgres',
-});
+const { PORT } = require('./util/config')
+const { connectToDatabase } = require('./util/db')
 
-class Note extends Model { } Note.init({ id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true }, content: { type: DataTypes.TEXT, allowNull: false }, important: { type: DataTypes.BOOLEAN }, date: { type: DataTypes.DATE } }, { sequelize, underscored: true, timestamps: false, modelName: 'note' })
+const notesRouter = require('./controllers/notes')
 
-Note.sync()
+app.use(express.json())
 
-app.get('/api/notes', async (req, res) => {
-	const notes = await Note.findAll()
-	console.log(JSON.stringify(notes, null, 2))
-	res.json(notes)
-})
+app.use('/api/notes', notesRouter)
 
-app.post('/api/notes', async (req, res) => {
-	try {
-		const note = await Note.create(req.body)
-		return res.json(note)
-	} catch (error) {
-		return res.status(400).json({ error })
-	}
-})
+const start = async () => {
+	await connectToDatabase()
+	app.listen(PORT, () => {
+		console.log(`Server running on port ${PORT}`)
+	})
+}
 
-app.get('/api/notes/:id', async (req, res) => {
-	const note = await Note.findByPk(req.params.id)
-	if (note) {
-		console.log(note.toJSON())
-		res.json(note)
-	} else {
-		res.status(404).end()
-	}
-})
-
-app.put('/api/notes/:id', async (req, res) => {
-	const note = await Note.findByPk(req.params.id)
-	if (note) {
-		note.important = req.body.important
-		await note.save()
-		res.json(note)
-	} else {
-		res.status(404).end()
-	}
-})
-
-const PORT = process.env.PORT || 3001
-
-app.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}`)
-})
+start()
